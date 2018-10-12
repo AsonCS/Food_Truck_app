0ee3ff753affbb361be89adde2a303614765a8c6
Anderson Costa da Silva <42609750+AsonCS@users.noreply.github.com>
2018-10-12 15:41:29 -0300

    12-10

--- www/js/epeds.js
+++ www/js/epeds.js
@@ -1,20 +1,20 @@
+var formIntercept = (form, callback = (form) => {}) => {
+    callback(form);
+    form.reset();
+    return false;
+}
+
 var form_incped_quant_f = (incped_quant_f) => {
     alert('enviou pedido');
-    incped_quant_f.reset();
     history.back();
-    return false;
 }
 
 var form_fecped_pag = (fecped_pag) => {
     alert('fechou pedido');
-    fecped_pag.reset();
     history.back();
-    return false;
 }
 
 var form_cadastro_cad = (cadastro_cad) => {
-    cadastro_cad.reset();
     alert('usuario incluido');
     history.back();
-    return false;
 }
